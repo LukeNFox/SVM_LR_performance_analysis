8219705ed0b024b6b3351ad4e26b4c595188f729
Luke Fox <l.fox9@nuigalway.ie>
2019-11-29 16:22:58 +0000

    code altered to allow for testing and comaprison of algorithms

--- svm_linear_regression.py
+++ svm_linear_regression.py
@@ -1,10 +1,12 @@
 import sklearn
-from sklearn import linear_model
+from sklearn import linear_model, preprocessing
 from sklearn.svm import SVR
 from sklearn.metrics import max_error,mean_squared_error, mean_absolute_error, r2_score
 
 import matplotlib.pyplot as plt
 from math import sqrt
+
+from statistics import mean 
 
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import cross_val_predict
@@ -25,24 +27,27 @@ def prepareData():
     
     return x_train, x_test, y_train, y_test
 
-def svm(x_train,y_train):
-    model = SVR(kernel='linear')
+def svm(x_train,y_train,kernel,degree,gamma,epsilon):
+    model = SVR(kernel=kernel,degree=degree,gamma=gamma,epsilon=epsilon)
     model.fit(x_train,y_train)
     return model
 
-def normalise():
-    #TBC
-    return null
+def normalise(x_train, x_test):
+    scaler = preprocessing.StandardScaler().fit(x_train)
+    x_train = scaler.transform(x_train)
+    x_test = scaler.transform(x_test)
+    return x_train, x_test
 
 def linearReg(x_train,y_train):
     model = linear_model.LinearRegression()
     model.fit(x_train,y_train)
     return model
 
+
 def predict(model,x_test,y_test):
     print("----------------------------")
     accuracy = model.score(x_test,y_test)
-    print(accuracy)
+    print("Accuracy:", accuracy)
     
     predictions = model.predict(x_test)
     # The coefficients
@@ -63,13 +68,32 @@ def predict(model,x_test,y_test):
     print('Variance score: %.2f' % r2_score(y_test, predictions))
 
     print("----------------------------")
+    
+    return accuracy
 
+linearAvg = 0
+svmAvg = 0
+
+
+degree = 9
+epsilon = 0.5
+gamma = 'auto'
+kernel = 'linear'
+
+#for i in range(0,10):
 x_train, x_test, y_train, y_test = prepareData()
 
+x_train, x_test = normalise(x_train, x_test)
+
 linearModel = linearReg(x_train, y_train)
-predict(linearModel,x_test,y_test)
+lrAccuracy = predict(linearModel,x_test,y_test)
+#linearAvg = linearAvg + lrAccuracy
+#predict(linearModel,x_train,y_train)
 
-svmModel = svm(x_train, y_train)
-predict(svmModel,x_test,y_test)
+svmModel = svm(x_train, y_train, kernel,degree,gamma,epsilon)
+svmAccuracy = predict(svmModel,x_test,y_test)
+#svmAvg = svmAvg + svmAccuracy
+# predict(svmModel,x_train,y_train)
 
-
+# print("LR: " + str(linearAvg/10))
+# print("SVM: " + str(svmAvg/10))
